3461990235c29f7f98c8f7f1ad6d905a187dc1f0
chaewon <varivarit060412@gmail.com>
2026-06-23 09:51:15 +0900

    Feat: app(5) 추가, settings.py에 등록

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'community',
+    'games',
+    'recommendations',
+    'wishlists',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
