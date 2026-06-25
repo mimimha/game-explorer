ade68fd1bb88f77b1317a6b15c251d609ecd56c2
JANGMINHWA <als2612@naver.com>
2026-06-25 11:25:25 +0900

    fix/css 수정

--- backend/recommendations/urls.py
+++ backend/recommendations/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
          views.RecommendLogListView.as_view(), name='recommend-logs'),
     path('recommend/logs/<int:log_id>/',
          views.RecommendLogDetailView.as_view(), name='recommend-log-detail'),
+    path('recommend/logs/<int:log_id>/delete/',
+         views.RecommendLogDestroyView.as_view(), name='recommend-log-delete'),
 ]
